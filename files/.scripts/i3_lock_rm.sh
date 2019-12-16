4764932b57837c2deb2a1984f83f115b5756756a
Antonio Ochoa <antonhub00@gmail.com>
2019-12-16 02:39:20 -0600

    scripts path fixed in i3 config and i3_lock_rm.sh

--- files/.scripts/i3_lock_rm.sh
+++ files/.scripts/i3_lock_rm.sh
@@ -11,7 +11,7 @@ trap revert HUP INT TERM
 xset +dpms dpms 0 0 5
 scrot -d 1 /tmp/locking_screen.png
 convert -blur 0x8 /tmp/locking_screen.png /tmp/screen_blur.png
-convert -composite /tmp/screen_blur.png ~/Scripts/rick_and_morty_ls.png -gravity South -geometry -1x1200 /tmp/screen.png
+convert -composite /tmp/screen_blur.png ~/.scripts/rick_and_morty_ls.png -gravity South -geometry -1x1200 /tmp/screen.png
 #convert -composite /tmp/screen_blur.png /tmp/screen_blur.png -gravity South -geometry -1x1200 /tmp/screen.png
 i3lock -i /tmp/screen.png
 revert
