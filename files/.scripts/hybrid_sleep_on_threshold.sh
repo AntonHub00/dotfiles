a45d2d1562ea3679417fe7d297540ba4aa261adc
Antonio Ochoa <antonhub00@gmail.com>
2020-07-19 01:55:01 -0500

    hybrid_sleep_on_threshold.sh now send notify before "shutdown"

--- files/.scripts/hybrid_sleep_on_threshold.sh
+++ files/.scripts/hybrid_sleep_on_threshold.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-capacity=$(cat /sys/class/power_supply/BAT0/capacity)
-status=$(cat /sys/class/power_supply/BAT0/status)
-threshold=5
+THRESHOLD=5
 
-if [[ $status == "Discharging" && $capacity -le $threshold ]]; then
-    /usr/bin/systemctl hybrid-sleep
-fi
+set_action(){
+    capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+    status=$(cat /sys/class/power_supply/BAT0/status)
+
+    if [[ $status == "Discharging" && $capacity -le $THRESHOLD ]]; then
+        case $1 in
+            "message")
+                notify-send -u critical "Battery threshold reached." "System will automatically hybrid-sleep in 1 minute (unless you connect the charger)."
+                sleep 1m
+                ;;
+            "hybrid-sleep")
+                systemctl hybrid-sleep
+                ;;
+        esac
+    fi
+}
+
+set_action "message"
+set_action "hybrid-sleep"
