823dd96a5256df65352f1d6584b90fb12c93e02c
Antonio Ochoa <antonhub00@gmail.com>
2020-07-21 23:12:26 -0500

    Fixed hybrid_sleep_on_threshold.sh script
    Fixed: Notification not sent when running in background as root.

--- files/.scripts/hybrid_sleep_on_threshold.sh
+++ files/.scripts/hybrid_sleep_on_threshold.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 THRESHOLD=5
+MY_USER="antonhub"
+MY_USER_ID=$(id -u $MY_USER)
 
 set_action(){
     capacity=$(cat /sys/class/power_supply/BAT0/capacity)
@@ -9,7 +11,7 @@ set_action(){
     if [[ $status == "Discharging" && $capacity -le $THRESHOLD ]]; then
         case $1 in
             "message")
-                notify-send -u critical "Battery threshold reached." "System will automatically hybrid-sleep in 1 minute (unless you connect the charger)."
+                sudo -u $MY_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$MY_USER_ID/bus notify-send -u critical "Battery threshold reached." "System will automatically hybrid-sleep in 1 minute (unless you connect the charger)."
                 sleep 1m
                 ;;
             "hybrid-sleep")
