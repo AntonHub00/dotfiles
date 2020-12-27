f485e04914ae2701e61b3eb886d9100d1b1f078a
Antonio Ochoa <antonhub00@gmail.com>
2020-12-26 22:23:23 -0600

    Bug fixes, show current keyboard layout and battery status added dwm_xsetroot.sh

--- files/.scripts/dwm_xsetroot.sh
+++ files/.scripts/dwm_xsetroot.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+print_current_keyboard_layout() {
+    COMMAND=$(xset -q | grep LED | awk '{ print $10 }')
+
+    case "$COMMAND" in
+     "00000000") echo "US " ;;
+     "00001000") echo "US (intl) " ;;
+      *) echo "Unknown " ;;
+    esac
+}
+
 while true; do
     disk_root=$(df -h | awk '{ if ($6 == "/") print $4 }')" "
 
@@ -11,13 +21,15 @@ while true; do
 
     memory=$(free -h | awk '(NR==2){ print $3"/"$2 }')" "
 
-    battery_status=$(cat /sys/class/power_supply/BAT0/status)"% "
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
     battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)"% "
 
-    date=$(date +"%m-%d-%Y   %H:%M ")
+    date=$(date +"%m-%d-%Y   %H:%M ")
 
-    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s %s ] [ %s ]" "$disk_root" "$disk_home" "$temperature" "$volume" "$memory" "$battery_status" "$battery_capacity" "$date")
+    keyboard=$(print_current_keyboard_layout)
+
+    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s %s ] [ %s ]" "$keyboard" "$disk_root" "$disk_home" "$temperature" "$volume" "$memory" "$battery_status" "$battery_capacity" "$date")
 
     xsetroot -name "$output"
 
