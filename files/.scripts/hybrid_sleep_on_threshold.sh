6fc6816cb028ba7b19bdf015f37afd7af179c59a
Antonio Ochoa <antonhub00@gmail.com>
2020-12-28 00:03:02 -0600

    Comments added and refactoring in hybrid_sleep_on_threshold.sh
    Refactored to "work" with any user who have permission to execute
    hybrid-sleep without password.

--- files/.scripts/hybrid_sleep_on_threshold.sh
+++ files/.scripts/hybrid_sleep_on_threshold.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# The user (MY_USER) must have permission to execute hybrid-sleep without
+# password, for example:
+# antonhub ALL=(ALL) NOPASSWD:/usr/bin/systemctl hybrid-sleep
 
 THRESHOLD=5
+MY_DISPLAY=":0"
 MY_USER="antonhub"
 MY_USER_ID=$(id -u $MY_USER)
 
@@ -10,16 +15,16 @@ set_action(){
 
     if [[ $status == "Discharging" && $capacity -le $THRESHOLD ]]; then
         case $1 in
-            "message")
-                sudo -u $MY_USER DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$MY_USER_ID/bus notify-send -u critical "Battery threshold reached." "System will automatically hybrid-sleep in 1 minute (unless you connect the charger)."
+            "warning")
+                sudo -u $MY_USER DISPLAY=$MY_DISPLAY DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$MY_USER_ID/bus notify-send -u critical "Battery threshold reached." "System will automatically hybrid-sleep in 1 minute (unless you connect the charger)."
                 sleep 1m
                 ;;
             "hybrid-sleep")
-                systemctl hybrid-sleep
+                sudo /usr/bin/systemctl hybrid-sleep
                 ;;
         esac
     fi
 }
 
-set_action "message"
+set_action "warning"
 set_action "hybrid-sleep"
