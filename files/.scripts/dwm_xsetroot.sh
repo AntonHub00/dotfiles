64a6c6effd80281b3c2dae63daf8e7b8309dcdfa
Antonio Ochoa <antonhub00@gmail.com>
2021-06-24 07:19:39 -0500

    Change for current hardware

--- files/.scripts/dwm_xsetroot.sh
+++ files/.scripts/dwm_xsetroot.sh
@@ -15,21 +15,21 @@ while true; do
 
     disk_home=$(df -h | awk '{ if ($6 == "/home") print $4 }')" "
 
-    temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)" "
+    # temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)" "
 
     volume=$(amixer get Master | awk -F '[][]' 'END{ print $2 }')" "
 
     memory=$(free -h | awk '(NR==2){ print $3"/"$2 }')" "
 
-    battery_status=$(cat /sys/class/power_supply/BAT0/status)
+    # battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-    battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)"% "
+    # battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)"% "
 
     date=$(date +"%m-%d-%Y   %H:%M ")
 
     keyboard=$(print_current_keyboard_layout)
 
-    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s %s ] [ %s ]" "$keyboard" "$disk_root" "$disk_home" "$temperature" "$volume" "$memory" "$battery_status" "$battery_capacity" "$date")
+    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s %s ]" "$keyboard" "$disk_root" "$disk_home" "$volume" "$memory" "$date")
 
     xsetroot -name "$output"
 
