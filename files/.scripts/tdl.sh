881cc1909739b01b89bb2ddcb9de7c8770109858
Antonio Ochoa <antonhub00@gmail.com>
2023-01-02 18:45:03 -0600

    tdl.sh script modified

--- files/.scripts/tdl.sh
+++ files/.scripts/tdl.sh
@@ -4,10 +4,10 @@ session_name=$1
 
 tmux new-session -d -s $session_name
 
-tmux rename-window General
+tmux rename-window Code
 
-tmux new-window -t $session_name:1 -n Code
-tmux new-window -t $session_name:2 -n Run
+tmux new-window -t $session_name:1 -n Run
+# tmux new-window -t $session_name:2 -n Run
 
 tmux select-window -t $session_name:0
 
