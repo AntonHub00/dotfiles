e35e28eb135f134a0a63407262bea953a29dca43
Antonio Ochoa <antonhub00@gmail.com>
2024-02-17 21:26:18 -0600

    neovim: nvim-tree disabled (but still installed)

--- files/.config/nvim/lua/config/options.lua
+++ files/.config/nvim/lua/config/options.lua
@@ -1,6 +1,6 @@
 -- Disable netrw
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
