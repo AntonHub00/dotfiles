b8e1f73efffafefd2da0a1bcb789312fcc0594b5
Antonio Ochoa <antonhub00@gmail.com>
2024-02-18 17:21:12 -0600

    dwm_xsetroot.sh script updated

--- files/.scripts/dwm_xsetroot.sh
+++ files/.scripts/dwm_xsetroot.sh
@@ -11,27 +11,29 @@ print_current_keyboard_layout() {
 }
 
 while true; do
-    disk_root=$(df -h | awk '{ if ($6 == "/") print $4 }')" "
+    # disk_root=$(df -h | awk '{ if ($6 == "/") print $4 }')" "
 
-    disk_home=$(df -h | awk '{ if ($6 == "/home") print $4 }')" "
+    # disk_home=$(df -h | awk '{ if ($6 == "/home") print $4 }')" "
 
     # temperature=$(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)" "
 
     volume=$(amixer get Master | awk -F '[][]' 'END{ print $2 }')" "
 
-    memory=$(free -h | awk '(NR==2){ print $3"/"$2 }')" "
+    memory=$(free -h | awk '(NR==2){ print $3"/"$2 }')" 󰍛"
 
-    # battery_status=$(cat /sys/class/power_supply/BAT0/status)
+    battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
-    # battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)"% "
+    battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)"% "
 
-    date=$(date +"%m-%d-%Y   %H:%M ")
+    date=$(date +"%m-%d-%Y  ")
+    time=$(date +"%l:%M %p ")
 
     keyboard=$(print_current_keyboard_layout)
 
-    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s ] [ %s ] [ %s %s ]" "$keyboard" "$disk_root" "$disk_home" "$volume" "$memory" "$date")
+    output=$(printf "[ %s ] [ %s ] [ %s ] [ %s %s ] [ %s | %s ]" "$keyboard" "$memory" "$volume" "$battery_status" "$battery_capacity" "$date" "$time")
 
     xsetroot -name "$output"
+    # echo $output
 
     sleep 1
 done
