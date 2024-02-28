44c406e6cd9d2abedd05cbf2c3c5b56d34ec7a9a
Antonio Ochoa <antonhub00@gmail.com>
2024-02-28 02:33:36 -0600

    set_inputs.sh script updated (current hardware)

--- scripts/.scripts/set_inputs.sh
+++ scripts/.scripts/set_inputs.sh
@@ -7,20 +7,21 @@
 
 # The second string are setttings available for that specific device. And you
 # can see that settings with the following command:
-# xinput list-props "TPPS/2 IBM TrackPoint" ("TPPS/2 IBM TrackPoint" is the
+# xinput list-props "Elan Touchpad" ("Elan Touchpad" is the
 # device name).
 
 # Set mouse config
 # xinput --set-prop "Logitech USB Optical Mouse" "libinput Accel Profile Enabled" 0, 1
 xinput --set-prop "Logitech G203 LIGHTSYNC Gaming Mouse" "libinput Accel Profile Enabled" 0, 1
+xinput --set-prop "Mi Silent Mouse" "libinput Accel Profile Enabled" 0, 1
 
 # Set touchpad config
-# xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Profile Enabled" 0, 1
-# xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
-# xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "Elan Touchpad" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "Elan Touchpad" "libinput Tapping Enabled" 1
+# xinput --set-prop "Elan Touchpad" "libinput Accel Profile Enabled" 0, 1
 
 # Set trackpoint config
-# xinput --set-prop "TPPS/2 IBM TrackPoint" "libinput Accel Profile Enabled" 0, 1
+# xinput --set-prop "Elan TrackPoint" "libinput Accel Profile Enabled" 0, 1
 
 # Set keyboard config
 # The command below means the following:
@@ -37,4 +38,5 @@ setxkbmap -model pc105 -layout us,us -variant ,intl -option grp:alt_space_toggle
 # DELAY is time in miliseconds to start to repeat key while holding down.
 # CPS (Click Per Second). I usually set the default value.
 # xset r rate 200 25
-xset r rate 200 40
+# xset r rate 200 40
+xset r rate 300 25
