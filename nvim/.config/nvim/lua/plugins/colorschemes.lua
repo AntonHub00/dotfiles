20bd60525f9743dec6e843f9cb36d539b7eba52b
Antonio Ochoa <antonhub00@gmail.com>
2024-02-28 03:33:21 -0600

    neovim-colorschemes.lua: gruvbox config (original gruvbox theme)

--- nvim/.config/nvim/lua/plugins/colorschemes.lua
+++ nvim/.config/nvim/lua/plugins/colorschemes.lua
@@ -95,6 +95,13 @@ return {
     -- priority = 1000,
     lazy = true,
     config = function()
+      vim.g.gruvbox_material_background = 'hard'
+      -- vim.g.gruvbox_material_background = 'medium'
+      vim.g.gruvbox_material_foreground = 'original'
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_statusline_style = 'original'
+      vim.g.gruvbox_material_diagnostic_virtual_text = 'colored'
+      vim.g.gruvbox_material_better_performance = 1
       vim.cmd.colorscheme 'gruvbox-material'
     end
   },
