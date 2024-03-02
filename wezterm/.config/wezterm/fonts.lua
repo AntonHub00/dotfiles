e4109b0e4e85e28f1782fc56f12410b6e145f254
Antonio Ochoa <antonhub00@gmail.com>
2024-03-02 15:45:00 -0600

    wezterm: font size added

--- wezterm/.config/wezterm/fonts.lua
+++ wezterm/.config/wezterm/fonts.lua
@@ -1,10 +1,19 @@
 local wezterm = require 'wezterm'
 local module = {}
 
-function module.apply_to_config(config)
+function set_font(config)
   -- config.font = wezterm.font('CaskaydiaCove Nerd Font')
   config.font = wezterm.font('FiraCode Nerd Font')
   -- config.font = wezterm.font('JetBrainsMono Nerd Font')
+end
+
+function set_font_size(config)
+  config.font_size = 11.0
+end
+
+function module.apply_to_config(config)
+  set_font(config)
+  set_font_size(config)
 end
 
 return module
