08b3b3729e2bc23d48954a7aee5f7ec2d4207ec7
Antonio Ochoa <antonhub00@gmail.com>
2024-03-02 15:48:38 -0600

    scripts/monitors: xrandr command for each output and order matters (avoids crtc error)

--- scripts/.scripts/monitors/office.sh
+++ scripts/.scripts/monitors/office.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output DP1 --off
+xrandr --output DP1-8 --off
+xrandr --output DP1-9 --off
+xrandr --output DP2 --off
+xrandr --output DP3 --off
+xrandr --output DP4 --off
+xrandr --output VIRTUAL1 --off
+xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
