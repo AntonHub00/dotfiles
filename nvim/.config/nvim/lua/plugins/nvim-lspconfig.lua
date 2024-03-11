7abe1551b732942cd4c15b9e2158627386c69c3f
Antonio Ochoa <antonhub00@gmail.com>
2024-03-11 02:26:44 -0600

    neovim: LSP setup cleaned and simplified

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,82 +1,65 @@
 return {
-  -- LSP Configuration & Plugins
   'neovim/nvim-lspconfig',
   dependencies = {
-    -- Automatically install LSPs to stdpath for neovim
     { 'williamboman/mason.nvim', config = true },
     'williamboman/mason-lspconfig.nvim',
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    { "folke/neodev.nvim",       opts = {} },
 
-    -- Useful status updates for LSP
-    -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     { 'j-hui/fidget.nvim',       opts = {} },
-
-    -- Additional lua configuration, makes nvim stuff amazing!
-    'folke/neodev.nvim',
   },
   config = function()
-    --  This function gets run when an LSP connects to a particular buffer.
-    local on_attach = function(_, bufnr)
-      -- NOTE: Remember that lua is a real programming language, and as such it is possible
-      -- to define small helper and utility functions so you don't have to repeat yourself
-      -- many times.
-      --
-      -- In this case, we create a function that lets us more easily define mappings specific
-      -- for LSP related items. It sets the mode, buffer and description for us each time.
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = 'LSP: ' .. desc
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
 
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+        -- See `:help K` for why this keymap
+        map('K', vim.lsp.buf.hover, 'Hover Documentation')
+        map('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+        vim.keymap.set({ 'i' }, '<C-k>', vim.lsp.buf.signature_help,
+          { buffer = event.buf, desc = 'LSP: Signature Documentation' })
+
+        map('gr', vim.lsp.buf.references, '[g]o to [r]eferences')
+        map('gi', vim.lsp.buf.implementation, '[g]o to [i]mplementation')
+        map('gd', vim.lsp.buf.definition, '[g]o to [d]efinition')
+        map('gD', vim.lsp.buf.declaration, '[g]o to [D]declaration')
+        map('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
+        map('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
+        map('<leader>t', vim.lsp.buf.type_definition, '[t]ype definition')
+
+        vim.keymap.set({ 'n', 'x' }, '<leader>F', vim.lsp.buf.format,
+          { buffer = event.buf, desc = 'LSP: [F]ormat selection' })
+
+        vim.api.nvim_buf_create_user_command(event.buf, 'Format', function(_)
+          vim.lsp.buf.format()
+        end, { desc = 'LSP: Format current buffer with LSP' })
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = event.buf,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = event.buf,
+            callback = vim.lsp.buf.clear_references,
+          })
+        end
       end
+    })
 
-      -- See `:help K` for why this keymap
-      nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-      nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
-      vim.keymap.set({ 'i' }, '<C-k>', vim.lsp.buf.signature_help, { buffer = bufnr, desc = 'Signature Documentation' })
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      nmap('gr', vim.lsp.buf.references, '[g]o to [r]eferences')
-      nmap('gi', vim.lsp.buf.implementation, '[g]o to [i]mplementation')
-      nmap('gd', vim.lsp.buf.definition, '[g]o to [d]efinition')
-      nmap('gD', vim.lsp.buf.declaration, '[g]o to [D]declaration')
-      nmap('<leader>rn', vim.lsp.buf.rename, '[r]e[n]ame')
-      -- nmap('<leader>ca', function()
-      --   vim.lsp.buf.code_action { context = { only = { 'quickfix', 'refactor', 'source' } } }
-      -- end, '[C]ode [A]ction')
-      nmap('<leader>ca', vim.lsp.buf.code_action, '[c]ode [a]ction')
-      nmap('<leader>t', vim.lsp.buf.type_definition, '[t]ype definition')
-
-      nmap('<leader>d', vim.diagnostic.open_float, 'Open floating diagnostic message')
-
-      vim.keymap.set({ 'n', 'x' }, '<leader>F', vim.lsp.buf.format, { buffer = bufnr, desc = '[F]ormat selection' })
-
-      -- Lesser used LSP functionality
-      -- nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      -- nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-      -- nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-      -- nmap('<leader>wl', function()
-      --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      -- end, '[W]orkspace [L]ist Folders')
-
-      -- Create a command `:Format` local to the LSP buffer
-      vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-        vim.lsp.buf.format()
-      end, { desc = 'Format current buffer with LSP' })
-    end
-
-    -- mason-lspconfig requires that these setup functions are called in this order
-    -- before setting up the servers.
-    require('mason').setup()
-    require('mason-lspconfig').setup()
-
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. They will be passed to
-    --  the `settings` field of the server config. You must look up that documentation yourself.
-    --
-    --  If you want to override the default filetypes that your language server will attach to you can
-    --  define the property 'filetypes' to the map in question.
     local servers = {
       -- clangd = {},
       -- gopls = {},
@@ -89,36 +72,55 @@ return {
       eslint = {},
       html = { filetypes = { 'html', 'twig', 'hbs' } },
       lua_ls = {
-        Lua = {
-          workspace = { checkThirdParty = false },
-          telemetry = { enable = false },
-          -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-          -- diagnostics = { disable = { 'missing-fields' } },
+        -- cmd = {...},
+        -- filetypes { ...},
+        -- capabilities = {},
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                '${3rd}/luv/library',
+                unpack(vim.api.nvim_get_runtime_file('', true)),
+              },
+              -- If lua_ls is really slow on your computer, you can try this instead:
+              -- library = { vim.env.VIMRUNTIME },
+            },
+            completion = {
+              callSnippet = 'Replace',
+            },
+            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            -- diagnostics = { disable = { 'missing-fields' } },
+          },
         },
       },
     }
 
-    -- Setup neovim lua configuration
+    -- mason-lspconfig requires that this to be setup before setting up the servers.
+    require('mason').setup()
+
+    -- You can add other tools here that you want Mason to install
+    -- for you, so that they are available from within Neovim.
+    local ensure_installed = vim.tbl_keys(servers or {})
+    -- vim.list_extend(ensure_installed, {
+    --   'stylua', -- Used to format lua code
+    -- })
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
     require('neodev').setup()
 
-    -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-    -- Ensure the servers above are installed
-    local mason_lspconfig = require 'mason-lspconfig'
-    mason_lspconfig.setup {
-      ensure_installed = vim.tbl_keys(servers),
-    }
-    mason_lspconfig.setup_handlers {
-      function(server_name)
-        require('lspconfig')[server_name].setup {
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        }
-      end,
-    }
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      },
+    })
   end
 }
