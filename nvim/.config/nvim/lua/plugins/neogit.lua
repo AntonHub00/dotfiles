bdae754b3784817c440ea7ee3bdb873d95719d93
Antonio Ochoa <antonhub00@gmail.com>
2024-03-19 08:20:49 -0600

    neovim: telescope sorter changed for neogit

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -6,5 +6,11 @@ return {
     'sindrets/diffview.nvim',        -- optional - Diff integration
     'nvim-telescope/telescope.nvim', -- optional
   },
-  config = true
+  config = function()
+    require("neogit").setup({
+      telescope_sorter = function()
+        return require("telescope").extensions.fzf.native_fzf_sorter()
+      end,
+    })
+  end
 }
