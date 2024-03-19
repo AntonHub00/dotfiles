99481bdb979fcf3494dd751173e7cc1b65349912
Antonio Ochoa <antonhub00@gmail.com>
2024-03-18 18:02:49 -0600

    neovim: diffview plugin enabled and mappings added

--- nvim/.config/nvim/lua/plugins/diffview.lua
+++ nvim/.config/nvim/lua/plugins/diffview.lua
@@ -1,22 +1,26 @@
 return {
   'sindrets/diffview.nvim',
-  enabled = false,
+  enabled = true,
   config = function()
-    local openedDiffView = false
+    vim.keymap.set(
+      "n",
+      "<leader>gdo",
+      function() vim.cmd("DiffviewOpen") end,
+      { desc = '[g]it [d]iff [o]pen' }
+   )
 
-    local toggleDiffview = function()
-      require("diffview").setup {}
+    vim.keymap.set(
+      "n",
+      "<leader>gdc",
+      function() vim.cmd("DiffviewClose") end,
+      { desc = '[g]it [d]iff [c]lose' }
+    )
 
-      openedDiffView = not openedDiffView
-
-      if (openedDiffView) then
-        vim.cmd("DiffviewOpen")
-      else
-        vim.cmd("DiffviewClose")
-      end
-    end
-
-    vim.keymap.set("n", "<leader>gd", toggleDiffview, { desc = '[g]it [d]iff' })
-    vim.keymap.set("n", "<leader>gf", ":DiffviewToggleFiles<CR>", { desc = '[g]it [f]iles' })
+    vim.keymap.set(
+      "n",
+      "<leader>gdt",
+      function() vim.cmd("DiffviewToggleFiles") end,
+      { desc = '[g]it [d]iff [t]oggle files' }
+    )
   end,
 }
