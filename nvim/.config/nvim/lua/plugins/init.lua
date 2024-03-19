95a1349bf64d1b9c89f1f0e6ae1bbc0eb67cb2a4
Antonio Ochoa <antonhub00@gmail.com>
2024-03-18 18:01:54 -0600

    neovim: disable vim-fugitive refactored and disabled

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,7 +1,12 @@
 return {
   'tpope/vim-surround',
   'tpope/vim-repeat',
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
   'nvim-tree/nvim-web-devicons',
+  -- {
+  --   'tpope/vim-fugitive',
+  --   enabled = true,
+  --   dependencies = {
+  --     'tpope/vim-rhubarb',
+  --   }
+  -- },
 }
