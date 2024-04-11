70041c6686a507a078af05a1bb695e340c1d8e19
Antonio Ochoa <antonhub00@gmail.com>
2024-04-11 00:52:05 -0600

    wezterm: new fonts and default font changed

--- wezterm/.config/wezterm/fonts.lua
+++ wezterm/.config/wezterm/fonts.lua
@@ -1,22 +1,33 @@
 local wezterm = require 'wezterm'
-local module = {}
+local M = {}
 
-function set_font(config)
+local function set_font(config)
+  config.font = wezterm.font('Berkeley Mono')
   -- config.font = wezterm.font('CaskaydiaCove Nerd Font')
   -- config.font = wezterm.font('FiraCode Nerd Font')
   -- config.font = wezterm.font('JetBrainsMono Nerd Font')
-  -- config.font = wezterm.font('Berkeley Mono Trial')
   -- config.font = wezterm.font('MonoLisa Trial')
-  config.font = wezterm.font('Comic Code Ligatures')
+  -- config.font = wezterm.font('Comic Code Ligatures')
+  -- config.font = wezterm.font('Hack')
+  -- config.font = wezterm.font('IBM Plex Mono')
+  -- config.font = wezterm.font('Intel One Mono')
+  -- config.font = wezterm.font({
+  --   family = 'Monaspace Neon',
+  --   -- family = 'Monaspace Argon',
+  --   -- family = 'Monaspace Xenon',
+  --   -- family = 'Monaspace Radon',
+  --   -- family = 'Monaspace Krypton',
+  --   harfbuzz_features = { 'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig' }
+  -- })
 end
 
-function set_font_size(config)
+local function set_font_size(config)
   config.font_size = 11.0
 end
 
-function module.apply_to_config(config)
+function M.apply_to_config(config)
   set_font(config)
   set_font_size(config)
 end
 
-return module
+return M
