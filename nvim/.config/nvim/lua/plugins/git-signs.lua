d12e2b508f04c5083c394dbb7ffece401557b435
Antonio Ochoa <antonhub00@gmail.com>
2024-08-20 08:35:36 -0600

    neovim: add navigation keymaps for hunks (git-signs)

--- nvim/.config/nvim/lua/plugins/git-signs.lua
+++ nvim/.config/nvim/lua/plugins/git-signs.lua
@@ -1,4 +1,35 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts = {},
+  config = function()
+    local gitsigns = require('gitsigns')
+
+    gitsigns.setup {}
+
+    vim.keymap.set(
+      'n',
+      ']c',
+      function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gitsigns.nav_hunk('next')
+        end
+      end,
+      { desc = "Next hunk or next change" }
+    )
+
+    vim.keymap.set(
+      'n',
+      '[c',
+      function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gitsigns.nav_hunk('prev')
+        end
+      end,
+      { desc = "Previous hunk or previous change" }
+    )
+  end
 }
