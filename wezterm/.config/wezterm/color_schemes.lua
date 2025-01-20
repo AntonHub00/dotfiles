3d19368900dbabfc540d8d82f168b5bc6bcd9e04
Antonio Ochoa <antonhub00@gmail.com>
2025-01-20 17:46:38 -0600

    wezterm: dawnfox colorscheme

--- wezterm/.config/wezterm/color_schemes.lua
+++ wezterm/.config/wezterm/color_schemes.lua
@@ -18,10 +18,11 @@ function module.apply_to_config(config)
   -- config.color_scheme = 'EverforestLight (Gogh)'
   -- config.color_scheme = 'Oxocarbon Dark (Gogh)'
   -- config.color_scheme = 'Kanagawa (Gogh)'
-  config.color_scheme = 'Catppuccin Mocha'
+  -- config.color_scheme = 'Catppuccin Mocha'
   -- config.color_scheme = 'Catppuccin Macchiato'
   -- config.color_scheme = 'Catppuccin Latte'
   -- config.color_scheme = 'Poimandres'
+  config.color_scheme = 'dawnfox'
 end
 
 return module
