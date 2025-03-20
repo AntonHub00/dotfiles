16c4ae44042be35b667f7b693a10245b20ee49d1
Antonio Ochoa <antonhub00@gmail.com>
2025-03-19 22:22:57 -0600

    feat(neovim): more mappings added to harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -19,5 +19,10 @@ return {
     map("<leader>2", function() harpoon:list():select(2) end, "select 2")
     map("<leader>3", function() harpoon:list():select(3) end, "select 3")
     map("<leader>4", function() harpoon:list():select(4) end, "select 4")
+    map("<leader>5", function() harpoon:list():select(5) end, "select 5")
+    map("<leader>6", function() harpoon:list():select(6) end, "select 6")
+    map("<leader>7", function() harpoon:list():select(7) end, "select 7")
+    map("<leader>8", function() harpoon:list():select(8) end, "select 8")
+    map("<leader>9", function() harpoon:list():select(9) end, "select 9")
   end
 }
