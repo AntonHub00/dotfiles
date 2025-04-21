e55506f3f63265176f364c7bdc6f16aa1931d3e2
Antonio Ochoa <antonhub00@gmail.com>
2025-04-20 23:01:50 -0600

    feat(neovim): keymaps for window management, uncommented

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -15,18 +15,18 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Better window movement
--- vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', { silent = true })
--- vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', { silent = true })
--- vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', { silent = true })
--- vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', { silent = true })
+vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', { silent = true })
+vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', { silent = true })
+vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', { silent = true })
+vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', { silent = true })
 
 -- Resize window horizontally
--- vim.api.nvim_set_keymap('n', '<leader>wl', '10<C-w>>', { silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>wh', '10<C-w><', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wl', '10<C-w>>', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wh', '10<C-w><', { silent = true })
 
 -- Resize window vertically
--- vim.api.nvim_set_keymap('n', '<leader>wk', '10<C-w>+', { silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>wj', '10<C-w>-', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wk', '10<C-w>+', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>wj', '10<C-w>-', { silent = true })
 
 -- Paste in visual mode without clearing yank buffer
 vim.api.nvim_set_keymap('x', '<leader>p', '"_dP', { silent = true })
