cf07a65dc50273ca9cfa372297a0733604e479a6
Antonio Ochoa <antonhub00@gmail.com>
2025-11-17 17:23:56 -0600

    feat(neovim): Avante file type removed from render-markdown plugin

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,8 +1,8 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  ft = { "markdown", "Avante" },
+  ft = { "markdown" },
   opts = {
-    file_types = { "markdown", "Avante" },
+    file_types = { "markdown" },
   },
   keys = {
     {
