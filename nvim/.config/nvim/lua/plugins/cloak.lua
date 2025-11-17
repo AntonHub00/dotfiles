0139d0fd270a83a60a8a23d9f319fe9eef7b7b8a
Antonio Ochoa <antonhub00@gmail.com>
2025-11-17 14:28:52 -0600

    feat(neovim): enable cloak plugin

--- nvim/.config/nvim/lua/plugins/cloak.lua
+++ nvim/.config/nvim/lua/plugins/cloak.lua
@@ -1,7 +1,7 @@
 return {
   'laytan/cloak.nvim',
   opts = {
-    enabled = false,
+    enabled = true,
     cloak_character = '*',
     -- The applied highlight group (colors) on the cloaking, see `:h highlight`.
     highlight_group = 'Comment',
