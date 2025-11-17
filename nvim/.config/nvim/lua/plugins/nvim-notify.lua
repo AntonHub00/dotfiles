52043a8e658493fadf6291619dd4cdb88bd6adec
Antonio Ochoa <antonhub00@gmail.com>
2025-11-17 17:20:56 -0600

    feat(neovim): nvim-notify plugin removed

--- nvim/.config/nvim/lua/plugins/nvim-notify.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'rcarriga/nvim-notify',
-  config = function()
-    require('notify').setup()
-    vim.keymap.set('', '<Esc>', ":lua require('notify').dismiss()<CR>", { silent = true })
-  end,
-}
