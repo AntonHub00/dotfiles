129bd794d5a3e8547d277c7e78d883163c2bfa92
Antonio Ochoa <antonhub00@gmail.com>
2025-11-18 08:41:42 -0600

    fix(neovim): `mason-lspconfig` initialization

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,49 +1,50 @@
 return {
-  'mason-org/mason-lspconfig.nvim',
-  opts = {
-    ensure_installed = {
-      -- "clangd",
-      -- "pyright",
-      -- "rust_analyzer",
-      "gopls",
-      "templ",
-      "tailwindcss",
-      "marksman",
-      "bashls",
-      "jsonls",
-      "clangd",
-      "ts_ls",
-      -- "vtsls",
-      "eslint",
-      "html",
-      "cssls",
-      -- "angularls",
-      "yamlls",
-      -- "golangci_lint_ls",
-      "harper_ls",
-      "lua_ls",
-      "omnisharp",
-    },
-    -- This enables the LSPs for me (calls "vim.lsp.config" for each server)
-    automatic_enable = true,
-  },
-  dependencies = {
-    {
-      'mason-org/mason.nvim',
-      opts = {
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-          }
-        }
-      }
-    },
-    'neovim/nvim-lspconfig',
-  },
-  config = function ()
+	"mason-org/mason-lspconfig.nvim",
+	dependencies = {
+		{
+			"mason-org/mason.nvim",
+			opts = {
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
+				},
+			},
+		},
+		"neovim/nvim-lspconfig",
+	},
+	config = function()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				-- "clangd",
+				-- "pyright",
+				-- "rust_analyzer",
+				"gopls",
+				"templ",
+				"tailwindcss",
+				"marksman",
+				"bashls",
+				"jsonls",
+				"clangd",
+				"ts_ls",
+				-- "vtsls",
+				"eslint",
+				"html",
+				"cssls",
+				-- "angularls",
+				"yamlls",
+				-- "golangci_lint_ls",
+				"harper_ls",
+				"lua_ls",
+				"omnisharp",
+			},
+			-- This enables the LSPs for me (calls "vim.lsp.config" for each server)
+			automatic_enable = true,
+		})
+
 		-- vim.lsp.enable("roslyn_ls")
-		vim.lsp.enable("omnisharp")
-  end
+		-- vim.lsp.enable("omnisharp")
+	end,
 }
