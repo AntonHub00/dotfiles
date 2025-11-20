897a68bfe1c74d7c546bc6a568d4e260fef878c5
Antonio Ochoa <antonhub00@gmail.com>
2025-11-19 22:27:31 -0600

    feat(neovim): colorcolumn removed

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,8 +2,8 @@
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Inserts spaces instead of tabs
 vim.opt.expandtab = true
@@ -29,11 +29,11 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Enable mouse mode
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option make the OS clipboard to remain independent.
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 
 -- Split window below
 vim.o.splitbelow = true
@@ -49,28 +49,26 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Decrease update time
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-vim.opt.completeopt = 'menu,menuone,noinsert'
+vim.opt.completeopt = "menu,menuone,noinsert"
 
 vim.opt.termguicolors = true
 
-vim.opt.listchars = { tab = '→ ', trail = '•', nbsp = '␣' }
+vim.opt.listchars = { tab = "→ ", trail = "•", nbsp = "␣" }
 vim.opt.list = false
 
 vim.opt.wrap = true
 
-vim.opt.foldmethod = 'indent'
+vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 99
 
 -- vim.opt.cursorline = true
-
-vim.opt.colorcolumn = "80"
 
 vim.opt.diffopt:append({ "algorithm:histogram" })
 
@@ -82,11 +80,11 @@ vim.opt.winborder = "rounded"
 vim.opt.laststatus = 3
 
 vim.diagnostic.config({
-  -- virtual_lines = true,
-  virtual_text = true
-  -- virtual_text = {
-  --   current_line = true
-  -- }
+	-- virtual_lines = true,
+	virtual_text = true,
+	-- virtual_text = {
+	--   current_line = true
+	-- }
 })
 
 -- vim: ts=2 sts=2 sw=2 et
