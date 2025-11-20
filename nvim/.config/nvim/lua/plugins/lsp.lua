8025bf9f5d2ec64c50ae511ce2444dfbf5b4f7a5
Antonio Ochoa <antonhub00@gmail.com>
2025-11-19 22:14:28 -0600

    feat(neovim): installed all listed lsps; enable the ones to use

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -18,9 +18,8 @@ return {
 	config = function()
 		require("mason-lspconfig").setup({
 			ensure_installed = {
-				-- "clangd",
-				-- "pyright",
-				-- "rust_analyzer",
+				"pyright",
+				"rust_analyzer",
 				"gopls",
 				"templ",
 				"tailwindcss",
@@ -29,22 +28,41 @@ return {
 				"jsonls",
 				"clangd",
 				"ts_ls",
-				-- "vtsls",
+				"vtsls",
 				"eslint",
 				"html",
 				"cssls",
-				-- "angularls",
+				"angularls",
 				"yamlls",
-				-- "golangci_lint_ls",
+				"golangci_lint_ls",
 				"harper_ls",
 				"lua_ls",
 				"omnisharp",
 			},
 			-- This enables the LSPs for me (calls "vim.lsp.config" for each server)
-			automatic_enable = true,
+			automatic_enable = false,
 		})
 
+		vim.lsp.enable("pyright")
+		vim.lsp.enable("rust_analyzer")
+		vim.lsp.enable("gopls")
+		-- vim.lsp.enable("golangci_lint_ls")
+		vim.lsp.enable("templ")
+		vim.lsp.enable("tailwindcss")
+		vim.lsp.enable("marksman")
+		vim.lsp.enable("bashls")
+		vim.lsp.enable("jsonls")
+		vim.lsp.enable("clangd")
+		vim.lsp.enable("ts_ls")
+		-- vim.lsp.enable("vtsls")
+		vim.lsp.enable("eslint")
+		vim.lsp.enable("html")
+		vim.lsp.enable("cssls")
+		vim.lsp.enable("angularls")
+		vim.lsp.enable("yamlls")
+		vim.lsp.enable("harper_ls")
+		vim.lsp.enable("lua_ls")
+		vim.lsp.enable("omnisharp")
 		-- vim.lsp.enable("roslyn_ls")
-		-- vim.lsp.enable("omnisharp")
 	end,
 }
