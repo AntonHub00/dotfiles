a4a8ec651b08d23f529373e174c6455f1c5a1a8a
Antonio Ochoa <antonhub00@gmail.com>
2025-11-19 22:16:01 -0600

    feat(neovim): remove help keybinding and format file (oil plugin)

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,16 +1,11 @@
 return {
-  'stevearc/oil.nvim',
-  enabled = true,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons"
-  },
-  config = function()
-    require('oil').setup {
-      keymaps = {
-        ['gh'] = "actions.show_help"
-      }
-    }
-
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-  end,
+	"stevearc/oil.nvim",
+	enabled = true,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("oil").setup()
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+	end,
 }
