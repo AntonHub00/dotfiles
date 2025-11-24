7d76ecf0e5df9bf5a102b9f79f30b1b6a930120c
Antonio Ochoa <antonhub00@gmail.com>
2025-11-23 22:16:57 -0600

    feat(neovim): adds `cedarbaum/fugitive-azure-devops.vim` plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,13 +1,14 @@
 return {
-  'tpope/vim-surround',
-  'tpope/vim-repeat',
-  'nvim-tree/nvim-web-devicons',
-  'nvim-lua/plenary.nvim',
-  {
-    'tpope/vim-fugitive',
-    enabled = true,
-    dependencies = {
-      'tpope/vim-rhubarb',
-    }
-  },
+	"tpope/vim-surround",
+	"tpope/vim-repeat",
+	"nvim-tree/nvim-web-devicons",
+	"nvim-lua/plenary.nvim",
+	{
+		"tpope/vim-fugitive",
+		enabled = true,
+		dependencies = {
+			"tpope/vim-rhubarb",
+			"cedarbaum/fugitive-azure-devops.vim",
+		},
+	},
 }
