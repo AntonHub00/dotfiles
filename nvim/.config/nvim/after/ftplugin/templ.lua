63ab0120294e0921119a9330b76c7a818559e300
Antonio Ochoa <antonhub00@gmail.com>
2025-11-23 22:23:02 -0600

    feat(neovim): after > ftplugin for templ files (go)

---
+++ nvim/.config/nvim/after/ftplugin/templ.lua
@@ -0,0 +1,8 @@
+-- Inserts spaces instead of tabs
+vim.opt_local.expandtab = false
+-- Size of tabs in spaces (visually)
+vim.opt_local.tabstop = 4
+-- Size of tabs in spaces when indent with '<', '>' or '='
+vim.opt_local.shiftwidth = 0
+-- Size of tabs in insert mode
+vim.opt_local.softtabstop = 0
