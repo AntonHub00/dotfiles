b97836ee3e7bd60f448bb9755eb615bea1f0bc18
Antonio Ochoa <antonhub00@gmail.com>
2026-01-13 23:41:09 -0600

    feat(nvim): new chars for `indent-blankline` plugin

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -1,13 +1,17 @@
 return {
-  'lukas-reineke/indent-blankline.nvim',
-  enabled = false,
-  main = 'ibl',
-  config = function()
-    local ibl = require("ibl")
-    ibl.setup {
-      scope = {
-        enabled = true
-      }
-    }
-  end
+	"lukas-reineke/indent-blankline.nvim",
+	-- enabled = false,
+	main = "ibl",
+	config = function()
+		local ibl = require("ibl")
+		ibl.setup({
+			scope = {
+				enabled = false,
+			},
+			indent = {
+				char = "│",
+				tab_char = "│",
+			},
+		})
+	end,
 }
