33b8e6b139a046bdc38e08435d573a66f1bdb03f
Antonio Ochoa <antonhub00@gmail.com>
2026-05-03 16:16:32 -0600

    feat(neovim): comment out keymaps for window movement (ctrl + h/j/k/l)

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,42 +1,42 @@
 -- Easier exit from insert mode
-vim.api.nvim_set_keymap('i', 'jj', '<esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "jj", "<esc>", { noremap = true, silent = true })
 
 -- Easier exit from terminal mode
-vim.api.nvim_set_keymap('t', '<leader><Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<leader><Esc>", "<C-\\><C-n>", { noremap = true, silent = true })
 
 -- Easier turn off for search highlight
-vim.api.nvim_set_keymap('n', '<leader>n', ':noh<cr>', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>n", ":noh<cr>", { silent = true })
 
 -- Nop for space
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
--- Better window movement
-vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', { silent = true })
-vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', { silent = true })
-vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', { silent = true })
-vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', { silent = true })
+-- Better window movement (can interfere with default keymaps)
+-- vim.api.nvim_set_keymap('n', '<c-h>', '<c-w>h', { silent = true })
+-- vim.api.nvim_set_keymap('n', '<c-j>', '<c-w>j', { silent = true })
+-- vim.api.nvim_set_keymap('n', '<c-k>', '<c-w>k', { silent = true })
+-- vim.api.nvim_set_keymap('n', '<c-l>', '<c-w>l', { silent = true })
 
 -- Resize window horizontally
-vim.api.nvim_set_keymap('n', '<leader>wl', '10<C-w>>', { silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wh', '10<C-w><', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wl", "10<C-w>>", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wh", "10<C-w><", { silent = true })
 
 -- Resize window vertically
-vim.api.nvim_set_keymap('n', '<leader>wk', '10<C-w>+', { silent = true })
-vim.api.nvim_set_keymap('n', '<leader>wj', '10<C-w>-', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wk", "10<C-w>+", { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wj", "10<C-w>-", { silent = true })
 
 -- Paste in visual mode without clearing yank buffer
-vim.api.nvim_set_keymap('x', '<leader>p', '"_dP', { silent = true })
+vim.api.nvim_set_keymap("x", "<leader>p", '"_dP', { silent = true })
 
 -- Easier replacements
 -- vim.api.nvim_set_keymap('n', '<leader>ra', ':%s///gc<Left><Left><Left><Left>', {})     -- [r]eplace [a]ll in normal mode
 -- vim.api.nvim_set_keymap('n', '<leader>rc', ':%s/<C-r><C-w>//gc<Left><Left><Left>', {}) -- [r]eplace [c]urrent word in cursor
 -- vim.api.nvim_set_keymap('x', '<leader>ra', ':s///gc<Left><Left><Left><Left>', {})      -- [r]eplace [a]ll in selection
 
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- Open netrw in the left
